6a43b35b86c0d181ab43eb933d67b284313b31b3
Dan1ell1 <daniellisz.silva@gmail.com>
2026-06-17 14:58:57 -0300

    feat: restaura contexto das tab

--- background.js
+++ background.js
@@ -1,18 +1,34 @@
-// lê as tabs abertas
-const tabs = await chrome.tabs.query({currentWindow: true});
-// cria objeto de contexto com info das tabs
-const context = {
-    name: "contexto",
-    savedAt: new Date().toISOString(),
-    tabs: tabs.map(tab => ({
-        url: tab.url,
-        title: tab.title,
-        pinned: tab.pinned
-    })),
-};
+async function saveContext() {
+    
+    // lê as tabs abertas
+    const tabs = await chrome.tabs.query({currentWindow: true});
+    // cria objeto de contexto com info das tabs
+    const context = {
+        name: "contexto",
+        savedAt: new Date().toISOString(),
+        tabs: tabs.map(tab => ({
+            url: tab.url,
+            title: tab.title,
+            pinned: tab.pinned
+        })),
+    };
 
-// salva contexto no armazenamento local
-const storage = await chrome.storage.local.get("contexts");
-const contexts = storage.contexts || [];
-contexts["contexto"] = context;
-await chrome.storage.local.set({contexts})
+    // salva contexto no armazenamento local
+    const storage = await chrome.storage.local.get("contexts");
+    const contexts = storage.contexts || [];
+    contexts["contexto"] = context;
+    await chrome.storage.local.set({contexts})
+}
+
+async function restoreContext() {
+    
+    //lê os contextos salvos
+    const getTabs = await chrome.storage.local.get("contexts");
+    const contexts = getTabs.contexts || {};
+    for (const tab of contexts["contexto"].tabs) {
+        chrome.tabs.create({
+            url: tab.url,
+            pinned: tab.pinned
+        })
+    };
+}
